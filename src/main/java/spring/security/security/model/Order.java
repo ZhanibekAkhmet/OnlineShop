0b6ef741909394c4860ff94a6a64131a21e3fe64
ZhanibekAkhmet <dzhanik61@gmail.com>
2023-07-17 19:08:48 +0600

    order

---
+++ src/main/java/spring/security/security/model/Order.java
@@ -0,0 +1,32 @@
+package spring.security.security.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+@Entity
+@Data
+@Table(name = "orders")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<OrderItem> orderItems;
+
+    private BigDecimal total;
+    private LocalDateTime createdDate;
+    @ManyToOne
+    private User user;
+    @Column(name = "status")
+    private int status;
+}
